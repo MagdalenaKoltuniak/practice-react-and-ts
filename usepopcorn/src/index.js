64e0064cf8895e2e570e4c39c96e385132e2f6f1
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-08-10 10:45:52 +0200

    add static version of usepopcorn app

---
+++ usepopcorn/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './components/App';
+
+import StarRating from './components/StarRating';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
+    <StarRating maxRating={10} size={24} color='red' defaultRating={3}/>
+    <StarRating />
+  </React.StrictMode>
+);
+
