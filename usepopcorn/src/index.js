55be7b0d4657fd921959e9b2f1d6d9f60f18bf85
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-08-14 18:02:22 +0200

    feat: add multiple new useEffect hooks

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './components/App';
-
-import StarRating from './components/StarRating';
+import './index.css';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
-    <StarRating maxRating={10} size={24} color='red' defaultRating={3}/>
-    <StarRating />
+    <App />
   </React.StrictMode>
 );
 
